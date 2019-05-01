120c9972d43e3b452a80158456e3e811bfc2d2d2
marks <markswearingen@bendcable.com>
2019-05-01 09:06:20 -0700

    Scaffold

---
+++ README.md
@@ -0,0 +1,52 @@
+# Project Name
+
+**Author**: Michael Chapman | Mark Swearingen
+**Version**: 1.0.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+This application is for users to put in and API key and location data in order to retrieve information from various APIs
+
+## Getting Started
+<!-- What are the steps that a user must take in order to build this app on their own machine and get it running? -->
+
+## Architecture
+This application is built using Node and Express. We are using the Google Maps API, Yelp API, Dark Sky API, Event Brite API, Meetups API, and Weather API.
+
+## Change Log
+04-30-2029 9:04pm - Website is fully scaffolded.
+
+## Credits and Collaborations
+Mark Swearingen
+
+###################
+Number and name of feature: Scaffold site
+
+Estimate of time needed to complete: 30 minutes
+
+Start time: 8:50
+
+Finish time: 9:05
+
+Actual time needed to complete: 15 minutes
+
+###################
+Number and name of feature: Location
+
+Estimate of time needed to complete: 3 hours
+
+Start time: 9:05
+
+Finish time: 10:30
+
+Actual time needed to complete: 1.5 hours
+
+###################
+Number and name of feature: Weather
+
+Estimate of time needed to complete: 3 hours
+
+Start time: 10:30
+
+Finish time: 12:00
+
+Actual time needed to complete: 1.5 hours
---
+++ server.js
@@ -0,0 +1,74 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(cors());
+
+app.get('/location', (request, response) => {
+  try {
+    let locationData = request.query.data;
+    let latLong = convertLatLong(locationData);
+    response.send(latLong);
+  } catch(e) {
+    let message = handleErrors(e);
+    response.status(message.status).send(message.responseText);
+  }
+});
+
+function convertLatLong(query){
+  let geoData = require('./data/geo.json');
+  let location = {
+    search_query: query,
+    formatted_query: geoData.results[0].formatted_address,
+    latitude: geoData.results[0].geometry.location.lat,
+    longitude: geoData.results[0].geometry.location.lng,
+  };
+  return location;
+}
+
+app.get('/weather', (request, response) =>{
+  try {
+    let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let month = ['Jan', 'Feb', 'Mar', 'Apl', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    let darksky = require('./data/darksky.json');
+    let result = [];
+
+    darksky.daily.data.forEach(object => {
+      let date = new Date(object.time * 1000);
+      let time = [days[date.getDay()], month[date.getMonth()], date.getDate(), date.getFullYear()].join(' ');
+      let forecast = object.summary;
+
+      let info = getWeather(forecast, time);
+
+      result.push(info);
+    });
+
+    response.send(result);
+  } catch(e) {
+    let message = handleErrors(e);
+    response.status(message.status).send(message.responseText);
+  }
+});
+
+function getWeather(forecast, time){
+  let weatherInfo = {
+    forecast: forecast,
+    time: time
+  };
+
+  return weatherInfo;
+}
+
+function handleErrors() {
+  let errObj = {
+    status: 500,
+    responseText: 'Sorry something went wrong',
+  };
+  return errObj;
+}
+
+app.listen(PORT);
