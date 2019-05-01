522cda8f56d688cabf6ba02d56c2a576297abcd8
Matt Wilkin <mwilkin22@gmail.com>
2019-05-01 10:18:27 -0700

    add data directory and data files.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+// const superagent =  require('superagent');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
